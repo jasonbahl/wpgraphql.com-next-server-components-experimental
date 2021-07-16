ded46b8ffa75a9e9dfe1d5393035bd101fca2f8f
Jason Bahl <jasonbahl@mac.com>
2021-07-16 15:35:41 -0600

    - remove unused code

--- components/TemplateRouter.server.js
+++ components/TemplateRouter.server.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { fetch } from 'react-fetch'
-import Tag from "./Tag";
 import ClientPage from "./Page.client";
 import ClientPost from "./Post.client";
 import SearchField from "./SearchField.client";
@@ -8,17 +7,13 @@ const wpEndpoint = process.env.NEXT_PUBLIC_WORDPRESS_GRAPHQL_API;
 
 export default function TemplateRouter({ searchText, location }) {
 
-    console.log( `${wpEndpoint}?query=query SearchPosts($search:String){posts(where:{search:$search}){nodes{id,title}}}&variables={"search":"${searchText}"}` );
-
     const response = fetch(`${wpEndpoint}?query=query SearchPosts($search:String){posts(where:{search:$search}){nodes{id,title}}}&variables={"search":"${searchText}"}`).json()
 
     switch (searchText) {
         case 'Page':
             return <ClientPage />
-            break;
         case 'Post':
             return <ClientPost />
-            break;
         default:
             return (
                 <div>
@@ -31,37 +26,4 @@ export default function TemplateRouter({ searchText, location }) {
             )
     }
 
-    // console.log( response )
-    // console.log( `location yo ${location}`)
-    // console.log( `search text yo ${searchText}`)
-    //
-    // return (
-    //     <div>
-    //         <h3>Gaa!!!</h3>
-    //         <SearchField />
-    //         <pre>{JSON.stringify(searchText, null, 2 ) }</pre>
-    //         <h3>Response</h3>
-    //         <pre>{ JSON.stringify(response, null, 2 ) }</pre>
-    //     </div>
-    // );
-
-    // return notes.length > 0 ? (
-    //     <ul className="notes-list">
-    //         {notes.map(note =>
-    //             note &&
-    //             (!searchText ||
-    //                 note.title.toLowerCase().includes(searchText.toLowerCase())) ? (
-    //                 <li key={note.id}>
-    //                     <SidebarNote note={note} />
-    //                 </li>
-    //             ) : null
-    //         )}
-    //     </ul>
-    // ) : (
-    //     <div className="notes-empty">
-    //         {searchText
-    //             ? `Couldn't find any notes titled "${searchText}".`
-    //             : 'No notes created yet!'}{' '}
-    //     </div>
-    // )
 }

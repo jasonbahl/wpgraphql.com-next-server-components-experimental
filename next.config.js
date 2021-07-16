b0d1c90b30a6649a8c648c2114efbd2a87d451a1
Jason Bahl <jasonbahl@mac.com>
2021-07-16 10:12:39 -0600

    - reset to Next Starter

--- next.config.js
+++ next.config.js
@@ -1,9 +1,37 @@
-const withWordPress = require("./plugins/WordPress");
+const ReactServerWebpackPlugin = require('react-server-dom-webpack/plugin')
+const fs = require('fs')
 
-module.exports = withWordPress({
-  reactStictMode: true,
-  i18n: {
-    locales: ['en-US', 'fr', 'es-mx'],
-    defaultLocale: 'en-US'
+let manifest
+class CopyReactClientManifest {
+  apply(compiler) {
+    compiler.hooks.emit.tapAsync(
+      'CopyReactClientManifest',
+      (compilation, callback) => {
+        const asset = compilation.assets['react-client-manifest.json']
+        const content = asset.source()
+        // there might be multiple passes (?)
+        // we keep the larger manifest
+        if (manifest && manifest.length > content.length) {
+          callback()
+          return
+        }
+        manifest = content
+        fs.writeFile('./libs/react-client-manifest.json', content, callback)
+      }
+    )
   }
-})
+}
+
+module.exports = {
+  experimental: {
+    reactMode: 'concurrent',
+  },
+  api: {
+    bodyParser: false,
+  },
+  webpack: config => {
+    config.plugins.push(new ReactServerWebpackPlugin({ isServer: false }))
+    config.plugins.push(new CopyReactClientManifest())
+    return config
+  },
+}

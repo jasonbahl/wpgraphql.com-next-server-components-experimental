6b3a4fce8b72636e8752d27c01872151c4167f42
Jason Bahl <jasonbahl@mac.com>
2021-07-16 16:46:25 -0600

    update templaterouter

--- components/TemplateRouter.server.js
+++ components/TemplateRouter.server.js
@@ -5,25 +5,32 @@ import ClientPost from "./Post.client";
 import SearchField from "./SearchField.client";
 const wpEndpoint = process.env.NEXT_PUBLIC_WORDPRESS_GRAPHQL_API;
 
-export default async function TemplateRouter({ searchText, location }) {
+export default function TemplateRouter({ searchText, location }) {
 
-    const response = await fetch(`${wpEndpoint}?query=query SearchPosts($search:String){posts(where:{search:$search}){nodes{id,title}}}&variables={"search":"${searchText}"}`).json()
+    const response = fetch(`${wpEndpoint}?query=query SearchPosts($search:String){posts(where:{search:$search}){nodes{id,title}}}&variables={"search":"${searchText}"}`).json();
+
+    let template;
 
     switch (searchText) {
         case 'Page':
-            return <ClientPage />
+            template = <ClientPage />
+            break;
         case 'Post':
-            return <ClientPost />
+            template =  <ClientPost />
+            break;
         default:
-            return (
+            response && response.data ? template = (
                 <div>
                     <h3>Gaa!!!</h3>
                     <SearchField />
                     <pre>{JSON.stringify(searchText, null, 2 ) }</pre>
                     <h3>Response</h3>
-                   <pre>{ JSON.stringify(response, null, 2 ) }</pre>
+                    <pre>{ JSON.stringify(response, null, 2 ) }</pre>
                 </div>
-            )
+            ) : <>Loading...</>;
+            break;
     }
+
+    return template
 
 }
